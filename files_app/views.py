a72c51b17fa914f1e19d2f23e4aacd311ab2254d
Jared Lemler <jaredtlemler@gmail.com>
2025-02-11 21:58:36 -0500

    Replacing our mock data

--- files_app/views.py
+++ files_app/views.py
@@ -1,17 +1,23 @@
 from django.http import HttpResponse
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import  render
+from .models import File
 
-files_data = [
-    {'id': 0, 'name': 'image1.jpeg', 'type': 'jpeg'},
-    {'id': 1, 'name': 'notes.txt', 'type': 'txt'},
-    {'id': 2, 'name': 'image2.jpeg', 'type': 'jpeg'}
-]
+# def files(request):
+#     data = File.objects.all()
+#     return render(request, 'files_app/files.html', {'files': data})
+
+# files_data = [
+#     {'id': 0, 'name': 'image1.jpeg', 'type': 'jpeg'},
+#     {'id': 1, 'name': 'notes.txt', 'type': 'txt'},
+#     {'id': 2, 'name': 'image2.jpeg', 'type': 'jpeg'}
+# ]
 
 def home(request):
     return HttpResponse("Hello there")
 def files_app(request):
-    return render(request, 'files/files.html', {'files': files_data})
+    data = File.objects.all()
+    return render(request, 'files/files.html', {'files_app': data})
 # def file_file(request, file_id):
 #     f = next((item for item in files_data if item['id'] == file_id), None)
 #     print("five", f)
@@ -20,7 +26,7 @@ def files_app(request):
 #     return Http404
     # return render(request, "files/files.html", {'files': files_data})
 def file_file(request, file_id):
-    f = next((item for item in files_data if item['id'] == file_id), None)
+    f = File.objects.get(pk=file_id)
     if f is not None:
         return render(request, 'files/file_file.html', {'file': f})
     else:

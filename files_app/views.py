61802e60deabe1c40b60ac7d55911b0add500de0
JL037 <jaredtlemler@gmail.com>
2025-02-11 22:13:32 -0500

    Update views.py

--- files_app/views.py
+++ files_app/views.py
@@ -1,17 +1,19 @@
 from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render
+from .models import File
 
-files_data = [
-    {'id': 0, 'name': 'image1.jpeg', 'type': 'jpeg'},
-    {'id': 1, 'name': 'notes.txt', 'type': 'txt'},
-    {'id': 2, 'name': 'image2.jpeg', 'type': 'jpeg'}
-]
+# files_data = [
+#     {'id': 0, 'name': 'image1.jpeg', 'type': 'jpeg'},
+#     {'id': 1, 'name': 'notes.txt', 'type': 'txt'},
+#     {'id': 2, 'name': 'image2.jpeg', 'type': 'jpeg'}
+# ]
 
 def home(request):
     return HttpResponse("Hello there")
 def files_app(request):
-    return render(request, 'files/files.html', {'files': files_data})
+    data = File.objects.all()
+    return render(request, 'files/files.html', {'files': data})
 # def file_file(request, file_id):
 #     f = next((item for item in files_data if item['id'] == file_id), None)
 #     print("five", f)
@@ -20,7 +22,7 @@ def files_app(request):
 #     return Http404
     # return render(request, "files/files.html", {'files': files_data})
 def file_file(request, file_id):
-    f = next((item for item in files_data if item['id'] == file_id), None)
+    f = File.objects.get(pk=file_id)
     if f is not None:
         return render(request, 'files/file_file.html', {'file': f})
     else:
